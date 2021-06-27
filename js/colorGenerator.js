24f4999b6cf609a81add669942a502e8bde14dd9
nischalmudennavar <nischalmudennavar@gmail.com>
2021-06-27 20:07:44 +0530

    added audioeffects

--- js/colorGenerator.js
+++ js/colorGenerator.js
@@ -3,17 +3,20 @@ const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const btn = document.getElementById('color-btn');
 const color = document.getElementById('color');
 const bgcolor = document.getElementById('reflect-color');
+const audioChime = document.getElementById('chime')
 
 btn.addEventListener('click', () => {
 
     
-    
+    audioChime.play();
+
     let hexColor = '#';
     for(let i = 0; i < 6; i++) {
 
         hexColor += hex[getRandomNumber()]
     }
     color.textContent = hexColor;
+
     bgcolor.style.backgroundColor = hexColor;
 
 
